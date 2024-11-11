bcfa62a5538d42235626ffa2ca4ad95cbeb967ae
Pedro Escumalha <pedrosimoes93@gmail.com>
2024-11-11 22:36:48 +0100

    change color scheme

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -16,9 +16,9 @@ return require("lazy").setup({
     spec = {
         { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
         {
-            'EdenEast/nightfox.nvim',
+            'projekt0n/github-nvim-theme',
 		    config = function()
-		    	vim.cmd('colorscheme nightfox')
+		        vim.cmd('colorscheme github_dark_default')
 		    end
         },
         { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
